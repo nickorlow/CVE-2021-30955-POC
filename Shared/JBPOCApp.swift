e29f2f0e2ff23515b3c4722750c1822a6fab9182
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-02-28 15:48:10 -0600

    Initial Commit

---
+++ Shared/JBPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  JBPOCApp.swift
+//  Shared
+//
+//  Created by Nicholas Orlowsky on 2/28/22.
+//
+
+import SwiftUI
+
+@main
+struct JBPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
